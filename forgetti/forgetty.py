2797faa6e479cabeb9fc72b9c52091a1138a3bab
paf <pafischer1992@gmail.com>
2019-02-19 20:54:17 +0100

    add: requirements

--- forgetti/forgetty.py
+++ forgetti/forgetty.py
@@ -1,18 +1,25 @@
 from item import make_item, items
+import click
 
-def greet():
-    print("You currently have X items in your forgetti vault!")
-
+@click.command()
+@click.option('-a', '--add', help='Add a new note.')
 def add():    
     title = raw_input("What do you want to remember?")
     group = raw_input("Which group should it be assigned to?")
     
     make_item(title, group)
 
+@click.command()
+@click.option('-q', '--query', help='Query a note by group.')
 def query():
     print("Listing all items.")
     for item in items:
         print(item.title)
 
+@click.command()
+@click.option('-s', '--status', help='Get status of all the notes.')
+def status():
+        print('yep, havent done anything')
+
 if __name__ == "__main__":
-    greet()
+        status()
