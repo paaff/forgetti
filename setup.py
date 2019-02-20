bb18fff15213da59f76336186190bdbe57039f80
paf <pafischer1992@gmail.com>
2019-02-20 21:16:09 +0100

    add: setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='forgetti',
+    version='0.0.1',
+    py_modules=['forgetti'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        forgetti=forgetti:cli
+    ''',
+)
